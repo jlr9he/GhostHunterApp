1b7ed86b08d05494eb03fa8382ccd031e3e46f16
ScottPatterson4 <soccermiddy34568@aim.com>
2014-11-05 15:12:09 -0500

    Version 2: with buttons

--- gen/com/example/ghosthunter/R.java
+++ gen/com/example/ghosthunter/R.java
@@ -26,7 +26,7 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -35,11 +35,12 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int Enter_text_here=0x7f050004;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
-        public static final int send=0x7f050003;
+        public static final int scores=0x7f050005;
+        public static final int start=0x7f050003;
+        public static final int welcome=0x7f050004;
     }
     public static final class style {
         /** 
