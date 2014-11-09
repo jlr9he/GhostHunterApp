07ad4579847938f3c415e868d4fb228376c1fc3f
cbholmes <carolineholmes@d-172-25-253-158.dhcp.Virginia.EDU>
2014-11-09 14:47:01 -0500

    Buttons and stuff
    Added buttons to link to an empty game activity. Everything compiles
    but when I try to install/run it on the tablet eclipse claims to have
    installed it properly but the app does not appear on the device

--- src/com/example/ghosthunter/MainActivity.java
+++ src/com/example/ghosthunter/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;  
 
 public class MainActivity extends Activity {
@@ -15,6 +17,16 @@ public class MainActivity extends Activity {
 	    protected void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.activity_main); 
+	        
+	        Button st = (Button) findViewById(R.id.begin_game);
+	        st.setOnClickListener( new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+				
+					startActivity(new Intent("com.example.ghosthunter.GAMEACTIVITY"));
+				}
+			});
 	 }
 //	 
 //	 @Override 
